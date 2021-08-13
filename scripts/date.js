b1103f0df18531b547f92b82b6181420fc9fe62a
Anderson026 <anderson-ferreira22@hotmail.com>
2021-08-13 10:02:01 -0300

    Ajuste no cálculo da senha do pdv e 2k9

--- scripts/date.js
+++ scripts/date.js
@@ -7,7 +7,7 @@ const divPassword = document.querySelector(".divPassword");
 let date = new Date();
 let hour = date.getHours();
 let day = date.getDate();
-let month = date.getMonth() + 1;
+let month = date.getMonth();
 let year = date.getFullYear();
 
 
@@ -17,10 +17,10 @@ let year = date.getFullYear();
     let calculaPasswordLoja = (hour + day + month + year) - 2000;
     passwordLoja.innerHTML = `Senha do acesso ao Live loja: <strong>${calculaPasswordLoja}</strong>`;
 
-    let calculatePasswordConfig = (hour + day + month + year) - 75;
+    let calculatePasswordConfig = (hour + day + month + 1 + year) - 75;
     passwordConfig.innerHTML = `Senha do config do PDV e Concentrador: <strong>${calculatePasswordConfig}</strong>`;
 
-    let calculatePasswordCbc = (year - month - day - hour);
+    let calculatePasswordCbc = (year - (month + 1) - day - hour);
     passworCbc.innerHTML = `Senha do config CBC Manager 2k9: <strong>${calculatePasswordCbc}</strong>`;
     
     divPassword.classList.remove("ocultText");
